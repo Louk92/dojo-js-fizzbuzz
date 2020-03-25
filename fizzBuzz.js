69e66650977beb968696f4615f65e6660cc412d3
louk92 <loukman.baism@gail.com>
2020-03-25 12:26:48 +0100

    resolve fizzBuzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,5 +1,18 @@
-const fizzBuzz = () => {
-  // Start here
-};
+const fizzBuzz = (nombre) => {
+    if ((nombre % 3 === 0) && (nombre % 5 === 0)) {
+      console.log("fizzbuzz");
+  } else if (nombre % 3 === 0) {
+    console.log("fizz");
+  } else if (nombre % 5 === 0) {
+    console.log("buzz") ;
+  } else {
+    console.log(nombre)
+  }
+}
+
+console.log(8)
+console.log(fizzBuzz(12))
+console.log(fizzBuzz(10))
+console.log(fizzBuzz(30))
 
 module.exports = fizzBuzz;
